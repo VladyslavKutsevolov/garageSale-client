3696502a0eb4e5d67b7464b0d2a4963c6cd710dc
Vladyslav <kytsevolov@gmail.com>
2020-10-22 19:45:32 -0600

    added args to the saleitem story

--- src/stories/SaleItem.stories.js
+++ src/stories/SaleItem.stories.js
@@ -2,15 +2,21 @@ import React from 'react';
 import SaleItem from '../components/saleItem/SaleItem';
 import '../index.scss';
 
-
 export default {
-  title: 'Example/SaleCard',
+  title: 'Example/SaleItem',
   component: SaleItem,
   argTypes: {
     backgroundColor: { control: 'color' }
   }
 };
 
-const Template = args => <SaleItem />;
+const Template = args => <SaleItem {...args} />;
 
 export const saleItem = Template.bind({});
+
+saleItem.args = {
+  imageUrl: 'https://picsum.photos/300/210',
+  title: 'title',
+  description: 'description',
+  price: 10
+};
