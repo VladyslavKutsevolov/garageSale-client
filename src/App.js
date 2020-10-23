6be5676c6402e8bc508401adeacf8b7089487f34
Vladyslav <kytsevolov@gmail.com>
2020-10-22 19:44:56 -0600

    remove unecessary component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import MiniDrawer from './components/navbar/MiniDrawer';
-import SaleItemList from './components/saleItem/SaleItemList';
 
 const App = () => (
   <div className="App">
     <MiniDrawer />
-    <br></br>
-    <br></br>
-    <br></br>
-    <SaleItemList />
   </div>
 );
 
