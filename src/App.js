0dce78f20f1e284c9325e916871ac2baaeb76f22
Vladyslav <kytsevolov@gmail.com>
2020-10-25 12:54:39 -0600

    removed merger comments

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import MiniDrawer from './components/navbar/MiniDrawer';
-<<<<<<< HEAD
-// import SaleItemList from './components/saleItem/SaleItemList';
-=======
->>>>>>> feature/sale-item
 
 const App = () => (
   <div className="App">
     <MiniDrawer />
-<<<<<<< HEAD
-    <br />
-    <br />
-    <br />
-=======
->>>>>>> feature/sale-item
   </div>
 );
 
