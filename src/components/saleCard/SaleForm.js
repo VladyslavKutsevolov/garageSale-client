3be4d2c70f7bf1482c9ce85a3281b0a95a01d7c3
Vladyslav <kytsevolov@gmail.com>
2020-10-26 13:29:42 -0600

    create garage sale form and its story

--- src/components/saleCard/SaleForm.js
+++ src/components/saleCard/SaleForm.js
@@ -4,7 +4,15 @@ import axios from 'axios';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-import { Fab, Button, Modal } from '@material-ui/core';
+import {
+  Fab,
+  Button,
+  Modal,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select
+} from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 
 const rand = () => Math.round(Math.random() * 20) - 10;
@@ -30,6 +38,10 @@ const useStyles = makeStyles(theme => ({
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3)
   },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120
+  },
   actionButtons: {
     display: 'flex',
     marginTop: '1.2rem',
@@ -40,24 +52,33 @@ const useStyles = makeStyles(theme => ({
   },
   upload: {
     marginTop: '1.2rem'
+  },
+  uploadButtonControl: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  filename: {
+    marginLeft: '1rem'
   }
 }));
 
 const initialState = {
   title: '',
   description: '',
-  price: ''
+  city: '',
+  province: ''
 };
 
-const SaleItemForm = () => {
+const SaleForm = () => {
   const classes = useStyles();
   const [form, setForm] = useState(initialState);
-  const [productImg, setProductImg] = useState(null);
+  const [saleImg, setSaleImg] = useState(null);
   const [open, setOpen] = useState(false);
-  const [modalStyle] = React.useState(getModalStyle);
+  const [modalStyle] = useState(getModalStyle);
   const [fileName, setFileName] = useState('');
 
   const handleChange = ({ target }) => {
+    console.log(form);
     setForm({
       ...form,
       [target.name]: target.value
@@ -65,7 +86,7 @@ const SaleItemForm = () => {
   };
 
   const getImg = ({ target }) => {
-    setProductImg(target.files[0]);
+    setSaleImg(target.files[0]);
     setFileName(target.files[0].name);
   };
   const clearInputFields = () => {
@@ -84,12 +105,13 @@ const SaleItemForm = () => {
     e.preventDefault();
     const formData = new FormData();
 
-    formData.append('productImg', productImg);
+    formData.append('saleImg', saleImg);
     formData.append('title', form.title);
     formData.append('description', form.description);
-    formData.append('price', form.price);
+    formData.append('city', form.city);
+    formData.append('province', form.province);
 
-    axios.post('/products/new', formData);
+    axios.post('http://localhost:3001/sales/new', formData);
     clearInputFields();
     handleClose();
   };
@@ -107,46 +129,77 @@ const SaleItemForm = () => {
       >
         <div style={modalStyle} className={classes.paper}>
           <Typography variant="h6" gutterBottom>
-            Product Information
+            Create Garage Sale
           </Typography>
           <form onSubmit={handleSubmit} action="/product/new" method="POST">
             <TextField
               onChange={handleChange}
+              value={form.title}
               required
               name="title"
-              label="Product Title"
+              label="Title"
               fullWidth
             />
             <TextField
               onChange={handleChange}
+              value={form.description}
               name="description"
-              label="Product Description"
+              label="Description"
               fullWidth
             />
             <TextField
               onChange={handleChange}
-              name="price"
-              label="Product Price"
+              value={form.city}
+              name="city"
+              label="City"
               fullWidth
             />
+            <FormControl className={classes.formControl}>
+              <InputLabel id="select-province">Province</InputLabel>
+              <Select
+                labelId="select-province"
+                id="select-province"
+                name="province"
+                value={form.province}
+                onChange={handleChange}
+                autoWidth
+              >
+                <MenuItem value="Alberta">Alberta</MenuItem>
+                <MenuItem value="British Columbia">British Columbia</MenuItem>
+                <MenuItem value="Manitoba">Manitoba</MenuItem>
+                <MenuItem value="New Brunswick">New Brunswick</MenuItem>
+                <MenuItem value="Newfoundland and Labrador">
+                  Newfoundland and Labrador
+                </MenuItem>
+                <MenuItem value="Nova Scotia">Nova Scotia</MenuItem>
+                <MenuItem value="Prince Edward Island">
+                  Prince Edward Island
+                </MenuItem>
+                <MenuItem value="Quebec">Quebec</MenuItem>
+                <MenuItem value="Saskatchewan">Saskatchewan</MenuItem>
+              </Select>
+            </FormControl>
             <div className={classes.upload}>
               <label htmlFor="product-img">
-                <Fab
-                  color="secondary"
-                  size="small"
-                  component="span"
-                  aria-label="add"
-                  variant="extended"
-                >
-                  <AddIcon />
-                  Upload photo
-                </Fab>
+                <div className={classes.uploadButtonControl}>
+                  <Fab
+                    color="secondary"
+                    size="small"
+                    component="span"
+                    aria-label="add"
+                    variant="extended"
+                  >
+                    <AddIcon />
+                    Upload photo
+                  </Fab>
+                  <p className={classes.filename}>{fileName}</p>
+                </div>
                 <input
                   id="product-img"
                   name="product-img"
                   type="file"
                   onChange={getImg}
-                  // style={{ display: 'none' }}
+                  style={{ display: 'none' }}
                 />
               </label>
             </div>
@@ -159,7 +212,11 @@ const SaleItemForm = () => {
               >
                 Create Product
               </Button>
-              <Button variant="outlined" color="secondary">
+              <Button
+                onClick={handleClose}
+                variant="outlined"
+                color="secondary"
+              >
                 Cancel
               </Button>
             </div>
@@ -170,4 +227,4 @@ const SaleItemForm = () => {
   );
 };
 
-export default SaleItemForm;
+export default SaleForm;
