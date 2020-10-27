41dfc43164c4271519d35c0ff71c0bd5dd92d7e6
Vladyslav <kytsevolov@gmail.com>
2020-10-27 14:31:16 -0600

    replace faka data to server data

--- src/components/saleItem/saleItemList.js
+++ src/components/saleItem/saleItemList.js
@@ -1,51 +1,13 @@
 /* eslint-disable quotes */
 import React from 'react';
-import SaleItem from './SaleItem';
-import Carousel from '../carousel/Carousel'
+import { useStateData } from '../../context/appContext';
 
-const fakeProductData = [
-  {
-    id: 1,
-    title: 'Lava lamp',
-    price: 5.0,
-    product_summary: 'Lava lamp in good condition. Lava still works.',
-    image_url:
-      'https://upload.wikimedia.org/wikipedia/commons/f/f2/1990s_Mathmos_Astro.jpg'
-  },
-  {
-    id: 2,
-    title: 'Monstera Plant',
-    price: 12.0,
-    product_summary: 'A nice plant. Grows. Needs water (not included).',
-    image_url: 'https://picsum.photos/300/210'
-  },
-  {
-    id: 3,
-    title: 'Used Xbox',
-    price: 80.0,
-    product_summary:
-      "Comes with games. Selling because of son's bad behaviour.",
-    image_url: 'https://picsum.photos/300/210'
-  },
-  {
-    id: 4,
-    title: 'Scarf',
-    price: 22.5,
-    product_summary:
-      "Used it to keep my neck warm. No longer needed as my neck isn't cold now.",
-    image_url: 'https://picsum.photos/300/210'
-  },
-  {
-    id: 5,
-    title: 'Watch',
-    price: 8.99,
-    product_summary: 'Tells you the time. Needs a new battery',
-    image_url: 'https://picsum.photos/300/210'
-  }
-];
+import SaleItem from './SaleItem';
 
 const SaleItemList = () => {
-  const days = fakeProductData.map(product => (
+  const { state } = useStateData();
+
+  const days = state.saleData.map(product => (
     <SaleItem
       key={product.id}
       title={product.title}
@@ -53,9 +15,9 @@ const SaleItemList = () => {
       productSummary={product.product_summary}
       imageUrl={product.image_url}
     />
-    ));
+  ));
 
-    return days;
+  return days;
 };
 
 export default SaleItemList;
