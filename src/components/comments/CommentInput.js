83f9441ac9a1d0b4f62dd0893d5e6261c02b5406
jamesbrown5292 <jamesbrown5292@gmail.com>
2020-10-27 11:44:05 -0700

    add input field and demo route

--- src/components/comments/CommentInput.js
+++ src/components/comments/CommentInput.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -11,27 +12,44 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+// Set to bypass error. Need product Id from backend.
+const productId = 100;
+
+// TODO 1: This component will need user info: userId(from cookies or state?), user name(get from users table using userID), productId
+
 const CommentInput = () => {
   const classes = useStyles();
+
+  const [comment, setComment] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Comment posted successfully:", comment)
+
+    // TODO 2: Axios request to be implemented when backend routes build
+
+    // axios.post(`/products/${productId}/new`, comment)
+    //   .then(() => {
+    //     console.log("Comment posted successfully:", comment)
+    //   })
+    //   .catch(err => {
+    //     alert('Your comment could not be posted. Please try again.')
+    //   })
+
+      setComment('');
+  }
 
   return (
     <>
       <div>
-        <form className={classes.root} noValidate autoComplete="off">
+        <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit} action={`comments/${productId}/new`}  method="POST">
           <TextField
+            value={comment}
             id="filled-multiline-static"
             label="Write a comment..."
-            multiline
-            rows={2}
             variant="filled"
             fullWidth
-            onKeyPress={(e) => {
-              if (e.key === 'Enter') {
-                e.preventDefault();
-                console.log(e);
-                // write your functionality here
-              }
-            }}
+            onChange={(e) => setComment(e.target.value)}
           />
         </form>
       </div>
