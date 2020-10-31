b2199aefa7a9115fd96725e13dc25e6973a27fe3
jamesbrown5292 <jamesbrown5292@gmail.com>
2020-10-30 20:26:34 -0700

    notifications front end scaffold

--- src/components/Notifications/NotificationList.js
+++ src/components/Notifications/NotificationList.js
@@ -2,22 +2,33 @@ import React from 'react';
 import { Snackbar } from '@material-ui/core/';
 import { useStateData } from '../../context/appContext';
 
-const NotificationList = ({setNotificationState, notificationState, handleNotificationClose}) => {
-
+const NotificationList = ({ notificationState, handleNotificationClose }) => {
   const { state } = useStateData();
   const { vertical, horizontal, open } = notificationState;
-  console.log("state in notification list", state)
+  console.log('state in notification list', state);
+
   return (
     <>
-      <Snackbar
-        anchorOrigin={{ vertical, horizontal }}
-        open={open}
-        onClose={() => handleNotificationClose()}
-        message="I love snacks"
-        key={vertical + horizontal}
-      />
+      {state.notifications.length ? (
+        state.notifications.map(notification => (
+          <Snackbar
+            anchorOrigin={{ vertical, horizontal }}
+            open={open}
+            onClose={() => handleNotificationClose()}
+            message={`User ${notification.from} commented on ${notification.product}: ${notification.comment}`}
+          />
+        ))
+      ) : (
+        <Snackbar
+          anchorOrigin={{ vertical, horizontal }}
+          open={open}
+          onClose={() => handleNotificationClose()}
+          message="No new notifications"
+          key={vertical + horizontal}
+        />
+      )}
     </>
-  )
+  );
 };
 
 export default NotificationList;
