cc060e3dc413eb45aaab2f7097771a846483d09e
Vladyslav <kytsevolov@gmail.com>
2020-11-04 19:00:28 -0700

    fixed wrong className

--- src/components/saleCard/SaleCard.js
+++ src/components/saleCard/SaleCard.js
@@ -54,7 +54,7 @@ const SaleCard = ({
   return (
     <>
       <animated.div
-        class="card"
+        className="card"
         onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
         onMouseLeave={() => set({ xys: [0, 0, 1] })}
         style={{ transform: props.xys.interpolate(trans) }}
