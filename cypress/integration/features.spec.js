50a87554e4a8da1198510388e3eb97d7ec28c954
9boogie <jzero.youngs89@gmail.com>
2020-11-04 22:27:21 -0600

    Cypress for delete

--- cypress/integration/features.spec.js
+++ cypress/integration/features.spec.js
@@ -3,8 +3,8 @@ describe('Navigation', () => {
     cy.visit("/");
     cy.get('[data-testid=drawer-opener]').click();
     cy.get('[data-testid=login-opener]').click();
-    cy.get('input[name="username"]').type('admin');
-    cy.get('input[name="password"]').type('1245');
+    cy.get('input[name="username"]').type('tester');
+    cy.get('input[name="password"]').type('tester');
     cy.contains('button', 'LogIn').click();
   });
 
@@ -24,5 +24,20 @@ describe('Navigation', () => {
     cy.wait(300);
     cy.contains('button', 'PENDING');
   });
+
+  xit('owener of garege should delete the item', () => {
+    cy.get('[data-testid=my-sales]').click();
+    cy.get('[data-testid=delete-item]').last().click();
+    cy.contains('button', 'YES').click();
+    cy.contains('[data-testid=item-title]', 'Smart TV').should('not.exist');
+  });
+
+  it('owener of garege should delete the existing garage sale', () => {
+    cy.get('[data-testid=my-sales]').click();
+    cy.get('[data-testid=delete-garage]').click();
+    cy.contains('button', 'YES').click();
+    cy.contains('Jae Garage');
+  });
+
 
 });
